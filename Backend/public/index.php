c4888f7847d0f6f68d8085afc7352c46ec71b07b
Pedro Borges Pospichil <pedroborge3807@gmail.com>
2026-07-02 23:59:58 +0000

    fix: rotas

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -4,6 +4,9 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use Slim\Factory\AppFactory;
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 
--- Backend/src/routes.php
+++ Backend/src/routes.php
@@ -1,9 +1,12 @@
 <?php
 
 require_once __DIR__ . "/Controllers/PetController.php";
+require_once __DIR__ . "/Controllers/UsuarioController.php";
 
 return function ($app) {
-    $pet = new PetController();
 
-    $app->get('/pets', [$pet, 'index']);
+    $usuario = new UsuarioController();
+    $app->get('/usuarios', [$usuario, 'index']);
+    $app->post('/usuarios', [$usuario, 'store']);
+    $app->post('/login', [$usuario, 'login']);
 };
