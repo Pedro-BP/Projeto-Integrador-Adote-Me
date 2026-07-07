d6dbcf5b88edca6120b685c0226acc297d1b2552
Pedro-BP <pedroborge3807@gmail.com>
2026-07-07 19:20:09 -0300

    feat: adicionar páginas de Login e Cadastro, atualizar App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
+import Login from "./pages/Login";
 import Index from "./pages/Index";
-
+import Cadastro from "./pages/Cadastro";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Index />
+      <Cadastro />
     </>
   );
 }
