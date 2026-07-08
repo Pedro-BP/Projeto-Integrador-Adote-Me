e7503a30c66e3e290533f08d33d40a79d5ec1b8d
João Pedro Antunes De Souza <teddbrgamer77@gmail.com>
2026-07-08 00:06:38 +0000

    feat: Base da pagina Animais/Vitrine concluida

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,12 @@ import Header from "./components/Header";
 import Login from "./pages/Login";
 import Index from "./pages/Index";
 import Cadastro from "./pages/Cadastro";
+import Animais from "./pages/Animais";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Cadastro />
+      <Animais />
     </>
   );
 }
